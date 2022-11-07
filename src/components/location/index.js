5a7c413f7f71d19aeae59ec436abf155978ae361
QuangNgoVan <quangngovan98@gmail.com>
2022-11-07 07:43:40 +0700

    delete le an hoi

--- src/components/location/index.js
+++ src/components/location/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import LazyLoad from 'react-lazyload';
 
-import { anHoiLocation, tiecCuoiLocation, thanhHonLocation } from '../../config';
+import { tiecCuoiLocation, thanhHonLocation } from '../../config';
 import Sectiontitle from '../section-title';
-import story1 from '../../images/events/1.jpg';
+// import story1 from '../../images/events/1.jpg';
 import story2 from '../../images/events/2.jpg';
 import story3 from '../../images/events/3.jpg';
 
@@ -17,7 +17,7 @@ const Location = () => {
     <div id="event" className="service-area section-padding">
       <div className="container">
         <Sectiontitle section={t('WHEN_WHERE')} />
-        <div className="service-area-menu">
+        {/* <div className="service-area-menu">
           <div className="Ceremony-wrap">
             <div className="row">
               <div className="col-lg-5">
@@ -46,7 +46,7 @@ const Location = () => {
               </div>
             </div>
           </div>
-        </div>
+        </div> */}
         <div className="Ceremony-wrap">
           <div className="row row-sp">
             <div className="col-lg-7">
